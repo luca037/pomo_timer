db21a450c02ea18acc9f76016c6ce0eeb11a5f0d
luca-d <lucapiai98@proton.me>
2023-06-16 11:16:08 +0200

    Apportate piccole modifiche

--- src/pomo.cpp
+++ src/pomo.cpp
@@ -2,10 +2,9 @@
 
 #include "../include/SimplePomoTimer.h"
 
-using namespace std;
 
 // check if string contains only digits
-bool isdigit(const string& s) {
+bool isdigit(const std::string& s) {
     for (char c : s)
         if (!isdigit(c)) return false;
     return true;
@@ -13,17 +12,17 @@ bool isdigit(const string& s) {
 
 // ask for a number
 int init_time() {
-    for (string s; cin >> s; )
+    for (std::string s; std::cin >> s; )
         if (isdigit(s)) return stoi(s);
     return 0;
 }
 
 // ask to quit session
 bool end_session_asking() {
-    cout << "You done a pomo!" << endl
-         << "Do you want to end you session here? [y/n]" << endl 
-         << "(timer restarts automatically after the answer)" << endl;
-    for (char c; cin >> c; )
+    std::cout << "You done a pomo!" << std::endl
+         << "Do you want to end you session here? [y/n]" << std::endl 
+         << "(timer restarts automatically after the answer)" << std::endl;
+    for (char c; std::cin >> c; )
         if (c == 'y') return true; // end session
         else if (c == 'n') break;
     return false;
@@ -32,18 +31,18 @@ bool end_session_asking() {
 int main(int argc, char* argv[]) {
     // setup: work, break, pomo
     int w{}, b{}, p{};
-    cout << "Set work time: ";
+    std::cout << "Set work time: ";
     w = init_time();
-    cout << "Set break time: ";
+    std::cout << "Set break time: ";
     b = init_time();
-    cout << "How many pomos today? ";
+    std::cout << "How many pomos today? ";
     p = init_time();
 
     timer::SimplePomoTimer t{w, b, p, argv[1], argv[2]};
 
     // wait to start
-    cout << "Type 's' to start timer" << endl;
-    for (char c; cin >> c; ) if (c == 's') break;
+    std::cout << "Type 's' to start timer" << std::endl;
+    for (char c; std::cin >> c; ) if (c == 's') break;
 
     // start session
     while (1) {
@@ -51,7 +50,8 @@ int main(int argc, char* argv[]) {
         if (t.done()) break;
         else if (end_session_asking()) break;
     }
-    cout << "We're done for today. Good job" << endl;
+    std::cout << "We're done for today. Good job" << std::endl;
+
 
     return 0;
 }
